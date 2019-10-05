041067ae2fad29db0f8a4f2b7d6dcd960a249d09
david.hsu <david.hsu@icantw.com>
2019-10-05 17:03:14 +0800

    tablelayout 擺放x y 數完成

--- app/src/main/java/com/davidhsu/chart/MainActivity.kt
+++ app/src/main/java/com/davidhsu/chart/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.davidhsu.chart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextPageBtn.setOnClickListener {
+
+            val row = editRow.text.toString()
+            val column = editColumn.text.toString()
+
+            val intent = Intent(this, TableLayoutActivity::class.java).apply {
+                putExtra("row", row)
+                putExtra("column", column)
+            }
+            startActivity(intent)
+        }
     }
 }
