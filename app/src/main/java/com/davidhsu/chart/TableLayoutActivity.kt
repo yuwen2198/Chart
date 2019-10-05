dc02ea25027fede9d413bc9052a2a8d1108010dd
david.hsu <david.hsu@icantw.com>
2019-10-05 12:31:12 +0800

    first commit

---
+++ app/src/main/java/com/davidhsu/chart/TableLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.davidhsu.chart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author : david.hsu on 2019-10-05
+ */
+class TableLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
